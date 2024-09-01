2c8f2da7dc8e5fac76b6fd1fd905375215fadfbf
Leandro Diaz <leandrodiaz042@gmail.com>
2024-09-01 18:39:29 -0300

    feat: database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   'https://utfs.io/f/f0fea8de-d002-47c2-81f8-2099108d5cd5-z5iy6c.png',
   'https://utfs.io/f/44bb3b56-22b3-4237-91f7-b724fca66dac-z5li3g.png',
@@ -11,12 +13,17 @@ const mockImages = mockUrls.map((url, index) => ({
   url
 }))
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany()
+  console.log(posts)
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map(image => (
-          <div key={image.id} className="w-48">
+        {posts.map(p => (
+          <div key={p.id}>{p.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + '-' + index} className="w-48">
             <img src={image.url} />
           </div>
         ))}
